a162add2ebacb34cc2b3c6c74fccdf3b04d01932
vanshika0508 <vermvans582000@gmail.com>
2025-11-05 16:35:51 -0500

    User class

---
+++ Foodezon.Core/Models/User.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Foodezon.Core.Models
+{
+    public abstract class User : Base
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public UserRole Role { get; set; } = UserRole.Customer;
+
+        public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+
+        public virtual Cart Cart { get; set; } = new Cart();
+
+
+    }
+    public enum UserRole
+    {
+        Customer
+    }
+}
