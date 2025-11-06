5c5d05e5c87e51f6c5705e3f7dafe377ae7649d7
Aryan <sin14784@sheridancollege.ca>
2025-11-06 18:55:51 -0500

    Updated Category.cs

--- Foodezon.Core/Models/Category.cs
+++ Foodezon.Core/Models/Category.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Foodezon.Core.Models
 {
     public class Category
