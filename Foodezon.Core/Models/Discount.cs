ea728f38b8ed6122b889e5c71bca5e5c1f5377c5
Aryan <sin14784@sheridancollege.ca>
2025-11-06 18:54:18 -0500

    Created Discount.cs

---
+++ Foodezon.Core/Models/Discount.cs
@@ -0,0 +1,17 @@
+namespace Foodezon.Core.Models
+{
+    public class Discount
+    {
+        public int DiscountId { get; set; }
+
+        public string DiscountCode { get; set; }
+        
+        public decimal Percentage { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int DishId { get; set; }
+    }
+}
--- Foodezon.Core/obj/Debug/net9.0/Foodezon.Core.AssemblyInfo.cs
+++ Foodezon.Core/obj/Debug/net9.0/Foodezon.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Foodezon.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f5c0b435536fc0f823dd18410200fafa17754bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5843f755042f7755a558db63a8db807f3fccd23b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Foodezon.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Foodezon.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
