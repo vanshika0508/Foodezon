92f0d4e05932077468bcda8c9928100427e6c465
Aryan <sin14784@sheridancollege.ca>
2025-11-06 20:13:22 -0500

    Updated OrderItem.cs

--- Foodezon.Core/Models/OrderItem.cs
+++ Foodezon.Core/Models/OrderItem.cs
@@ -5,12 +5,20 @@ namespace Foodezon.Core.Models
 {
     public class OrderItem
     {
+        [Key]
         public int OrderItemId { get; set; }
 
+        [Required]
         public int OrderId { get; set; }
 
+        [Required]
         public int DishId { get; set; }
 
+        [Required]
         public int Quantity { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal UnitPrice { get; set; }
     }
 }
