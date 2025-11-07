3f74822fba249f7b964eb3bc379d8174616e8b8c
Aryan <sin14784@sheridancollege.ca>
2025-11-06 20:06:20 -0500

    Updated Discount.cs

--- Foodezon.Core/Models/Category.cs
+++ Foodezon.Core/Models/Category.cs
@@ -6,10 +6,13 @@ namespace Foodezon.Core.Models
 {
     public class Category
     {
+        [Key]
         public int CategoryId { get; set; }
-
+        
+        [Required, StringLength(50)]
         public string Name { get; set; }
 
+        [StringLength(200)]
         public string Details { get; set; }
     }
 }
--- Foodezon.Core/Models/Discount.cs
+++ Foodezon.Core/Models/Discount.cs
@@ -6,16 +6,23 @@ namespace Foodezon.Core.Models
 {
     public class Discount
     {
+        [Key]
         public int DiscountId { get; set; }
 
+        [Required, StringLength(50)]
         public string DiscountCode { get; set; }
 
+        [Required]
+        [Range(0,100)]
         public decimal Percentage { get; set; }
 
         public DateTime StartDate { get; set; }
 
         public DateTime EndDate { get; set; }
 
+        [Required]
         public int DishId { get; set; }
+
+        public bool ActiveStatus => DateTime.Now >= StartDate && DateTime.Now <= EndDate;
     }
 }
