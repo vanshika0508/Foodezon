c7b212791818ec7180fe4eb4387c937cc753ba87
vanshika0508 <vermvans582000@gmail.com>
2025-11-11 15:48:45 -0500

    user response

--- Foodezon.Infrastructure/obj/Debug/net9.0/Foodezon.Infrastructure.AssemblyInfo.cs
+++ Foodezon.Infrastructure/obj/Debug/net9.0/Foodezon.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Foodezon.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e67e3ca8e4755fb87a92d0b00255a3248f337774")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42069b6fc1953c9befb68d16cda5b2478d89c482")]
 [assembly: System.Reflection.AssemblyProductAttribute("Foodezon.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Foodezon.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
