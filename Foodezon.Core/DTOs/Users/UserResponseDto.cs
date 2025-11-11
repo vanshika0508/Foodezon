bb636ae55174c6a45c3849c5c481654ebced73d7
vanshika0508 <vermvans582000@gmail.com>
2025-11-11 16:00:32 -0500

    response

---
+++ Foodezon.Core/DTOs/Users/UserResponseDto.cs
@@ -0,0 +1,13 @@
+namespace Foodezon.Core.DTOs.Users
+{
+    public class UserResponseDto
+    {
+        public int Id { get; set; }
+        public string FirstName   { get; set; } = string.Empty;
+        public string LastName    { get; set; } = string.Empty;
+        public string Email       { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Address     { get; set; } = string.Empty;
+        public string Role        { get; set; } = string.Empty;
+    }
+}
