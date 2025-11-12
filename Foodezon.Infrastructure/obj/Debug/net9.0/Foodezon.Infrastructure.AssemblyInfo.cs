840bcf3347f18b98534845484cff0135f4d4e520
vanshika0508 <vermvans582000@gmail.com>
2025-11-11 19:38:40 -0500

    json file

--- Foodezon.Infrastructure/obj/Debug/net9.0/Foodezon.Infrastructure.AssemblyInfo.cs
+++ Foodezon.Infrastructure/obj/Debug/net9.0/Foodezon.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Foodezon.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51b679ec0707e75d823435c75904165ce12099cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e7702f813b70cbf080233a7bf4937a40b94e529")]
 [assembly: System.Reflection.AssemblyProductAttribute("Foodezon.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Foodezon.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
