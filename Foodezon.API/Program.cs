1616db30e6dfef428ebf5c70b84fae462d4180a0
vanshika0508 <vermvans582000@gmail.com>
2025-11-17 15:08:03 -0500

    program.cs updated

--- Foodezon.API/Program.cs
+++ Foodezon.API/Program.cs
@@ -20,6 +20,8 @@ builder.Services.AddScoped<ICartService, CartService>();
 
 builder.Services.AddScoped<IOrderRepository , OrderRepository>();
 builder.Services.AddScoped<IOrderService,OrderService>();
+
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddControllers();
 
 var app = builder.Build();
