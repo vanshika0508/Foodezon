123d14db23f287ad9953cb9df897fd3b4db479f4
Aryan <sin14784@sheridancollege.ca>
2025-11-18 20:29:06 -0500

    Completed FoodezonDbContext

--- Foodezon.Infrastructure/Data/FoodezonDbContext.cs
+++ Foodezon.Infrastructure/Data/FoodezonDbContext.cs
@@ -30,5 +30,30 @@ public class FoodezonDbContext : DbContext
             entity.Property(c => c.Name).IsRequired().HasMaxLength(50);
             entity.Property(c => c.Details).HasMaxLength(200);
         });
+
+        modelBuilder.Entity<Discount>(entity =>
+        {
+            entity.HasKey(d => d.DiscountId);
+            entity.Property(d => d.DiscountCode).IsRequired().HasMaxLength(30);
+            entity.Property(d => d.Percentage).IsRequired();
+            entity.HasOne<Dish>().WithMany().HasForeignKey(d => d.DishId).OnDelete(deleteBehavior : DeleteBehavior.Cascade);
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(o => o.OrderId);
+            entity.Property(o => o.CustomerName).IsRequired().HasMaxLength(100);
+            entity.Property(o => o.CustomerEmail).IsRequired().HasMaxLength(100);
+            entity.Property(o => o.OrderStatus).IsRequired().HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<OrderItem>(entity =>
+        {
+            entity.HasKey(oi => oi.OrderItemId);
+            entity.Property(oi => oi.Quantity).IsRequired();
+            entity.Property(oi => oi.UnitPrice).HasColumnType("decimal(10,2)").IsRequired();
+            entity.HasOne<Order>().WithMany().HasForeignKey(oi => oi.OrderId).OnDelete(DeleteBehavior.Cascade);
+            entity.HasOne<Dish>().WithMany().HasForeignKey(oi => oi.DishId).OnDelete(DeleteBehavior.Cascade);
+        });
     }
 }
