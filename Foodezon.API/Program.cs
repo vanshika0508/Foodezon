9d99226b32e4b488d2f7f06078878b6508d8d18f
vanshika0508 <vermvans582000@gmail.com>
2025-11-21 20:12:02 -0500

    updated program.cs

--- Foodezon.API/Program.cs
+++ Foodezon.API/Program.cs
@@ -22,11 +22,32 @@ builder.Services.AddScoped<IOrderRepository , OrderRepository>();
 builder.Services.AddScoped<IOrderService,OrderService>();
 
 builder.Services.AddScoped<ICategoryService, CategoryService>();
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSession();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
 app.MapControllers();
 
 app.Run();
