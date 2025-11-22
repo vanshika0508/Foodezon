fe88e8af6e42619366a196512b1c8308eb1e1ad4
vanshika0508 <vermvans582000@gmail.com>
2025-11-21 20:06:47 -0500

    unknown

--- Foodezon.API/obj/Debug/net9.0/Foodezon.API.AssemblyInfo.cs
+++ Foodezon.API/obj/Debug/net9.0/Foodezon.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Foodezon.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d344e2e2434901400a9200aa193708e6886b68d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b6eb40c9ed9898cc2a8bf5c20d8c17a09bb6a83")]
 [assembly: System.Reflection.AssemblyProductAttribute("Foodezon.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Foodezon.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
