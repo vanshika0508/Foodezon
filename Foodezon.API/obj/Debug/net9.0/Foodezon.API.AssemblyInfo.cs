da71c4dbe13cade9ac67c5ab44e7f655efbe17f7
vanshika0508 <vermvans582000@gmail.com>
2025-11-24 13:06:08 -0500

    cart view created

--- Foodezon.API/obj/Debug/net9.0/Foodezon.API.AssemblyInfo.cs
+++ Foodezon.API/obj/Debug/net9.0/Foodezon.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Foodezon.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+202533998832730606251879ec202f5bbd3be51d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ca7ba3d9282c935f668a55b8f2eab3fa18753e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Foodezon.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Foodezon.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
