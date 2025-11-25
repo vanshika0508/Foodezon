6f85e1c91887588dfed56e9aa0f585813fff360a
vanshika0508 <vermvans582000@gmail.com>
2025-11-24 19:39:20 -0500

    more changes to view of home page and unknown

--- Foodezon.API/obj/Debug/net9.0/Foodezon.API.AssemblyInfo.cs
+++ Foodezon.API/obj/Debug/net9.0/Foodezon.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Foodezon.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c14e1c91283d4e86662b0c0ec1ec5034cfa0082d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87b9372981014c4a2e111cfc1bcd27f9590f410b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Foodezon.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Foodezon.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
