f555d28593211980b78ee2dcac85a96eea7c9376
vanshika0508 <vermvans582000@gmail.com>
2025-11-26 12:17:35 -0500

    checkout page eddited

--- Foodezon.API/obj/Debug/net9.0/Foodezon.API.AssemblyInfo.cs
+++ Foodezon.API/obj/Debug/net9.0/Foodezon.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Foodezon.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4787171f31c75b4a8f39d20e5d73316b9ebe280")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40b11ffa2f47de1cab5fff3d3788750ce6a24335")]
 [assembly: System.Reflection.AssemblyProductAttribute("Foodezon.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Foodezon.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
