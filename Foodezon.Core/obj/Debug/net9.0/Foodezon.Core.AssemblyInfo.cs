e7d89f631b0bce07a50e224b9b86e861b711b4f3
Aryan <sin14784@sheridancollege.ca>
2025-11-27 00:37:25 -0500

    Created IAdminService.cs

--- Foodezon.Core/obj/Debug/net9.0/Foodezon.Core.AssemblyInfo.cs
+++ Foodezon.Core/obj/Debug/net9.0/Foodezon.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Foodezon.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2aee701ef68dff51811fd36c6e972cf01c7ad3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Foodezon.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Foodezon.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
