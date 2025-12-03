2c9ce2be093055637b19e5fa3cf51276552eaf5c
Aryan <sin14784@sheridancollege.ca>
2025-12-02 19:10:31 -0500

    Created Details.cshtml for Orders - Aryan

--- Foodezon.API/obj/Debug/net9.0/Foodezon.API.AssemblyInfo.cs
+++ Foodezon.API/obj/Debug/net9.0/Foodezon.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Foodezon.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67a890a260d54e282101af7027b1b516f0817460")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2ae4cacbf52ae20b8de97b0bb939326063314f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Foodezon.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Foodezon.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
